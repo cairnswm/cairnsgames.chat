7ed279e6a80c4ceefec13a1aa7d3757beb9bc235
cairnswm <cairnswm@gmail.com>
2023-03-12 17:48:20 -0400

    Fix build error config

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -5,10 +5,7 @@ module.exports = {
   filename: 'chat.js',
   exposes: {
     './chat': './src/components/chat.js'
-  },  r
-  emotes: {
-    cairnsgames: process.env.REACT_APP_CAIRNSGAMES_REMOTE
-  },
+  },  
   shared: {
     ...deps,
     react: { singleton: true, requiredVersion: deps.react },
