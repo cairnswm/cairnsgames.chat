173bc1344ca3aa8e462587e5acc83b79d0f1624e
cairnswm <cairnswm@gmail.com>
2023-03-12 16:14:36 -0400

    Chat Version 1

---
+++ modulefederation.config.js
@@ -0,0 +1,14 @@
+const deps = require('./package.json').dependencies
+
+module.exports = {
+  name: 'chat',
+  filename: 'chat.js',
+  exposes: {
+    './chat': './src/components/chat.js'
+  },
+  shared: {
+    ...deps,
+    react: { singleton: true, requiredVersion: deps.react },
+    'react-dom': { singleton: true, requiredVersion: deps['react-dom'] }
+  }
+}
---
+++ src/components/chat.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import { Form, Offcanvas, Row, Col } from "react-bootstrap";
+import { Send, Chat } from "react-bootstrap-icons";
+
+function ChatDialog({ system, start, apikey, onSend }) {
+  const [show, setShow] = useState(true);
+  const [msg, setMsg] = useState();
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [messages, setMessages] = useState([
+    {
+      role: "system",
+      content:system
+    },
+    {
+      role: "user",
+      content: start,
+    },
+  ]);
+
+
+  console.log("messages", messages);
+
+  const shortChatMessages = messages.filter((c) => c.role !== "system");
+  console.log("shortChatMessages", shortChatMessages);
+
+  const send = () => {
+    onSend(msg);
+  }
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        <Chat />
+      </Button>
+
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        style={{ margin: "50px", borderRadius: "20px" }}
+      >
+        <Offcanvas.Header
+          closeButton
+          style={{ borderBottom: "1px solid grey" }}
+        >
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          {shortChatMessages.map((message) => (
+            <Row>
+              <Col xs={3}>{message.role}</Col>
+              <Col>{message.content}</Col>
+            </Row>
+          ))}
+        </Offcanvas.Body>
+        <Offcanvas.Header>
+            <Form.Control type="text" placeholder="Enter message" value={msg} onChange={ev => setMsg(ev.target.value)} />
+          <Button variant="primary" onClick={send}>
+            <Send />
+          </Button>
+        </Offcanvas.Header>
+      </Offcanvas>
+    </>
+  );
+}
+
+export default ChatDialog;
